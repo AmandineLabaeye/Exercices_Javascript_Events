1704849f62f19a805e96818bfc39d4c12011dab0
sstienface <s.stien@fondationface.org>
2018-10-24 13:11:41 +0200

    Initial Commit

---
+++ exercice1.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+/* Modifier le code ci dessous */
+
+document.getElementById("cliquezmoi").("", function(){
+    alert('Click')
+});
+
+
---
+++ exercice2/exercice2.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+/* Modifier le code ci dessous */
+
+document.getElementById("bouton").("", function(){
+    alert('event déclenché')
+});
+
+
